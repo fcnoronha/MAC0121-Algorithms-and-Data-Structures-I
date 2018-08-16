080ea9241e077113b9d0a3a42c50a21ac6e2c7b7
mrfelipenoronha <felipe.castro.noronha@hotmail.com>
2018-08-16 19:45:01 -0300

    Melhorias de memoria e parametros ep1

--- ep1/ep1.c
+++ ep1/ep1.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
-#include <stdint.h>
-#include <math.h>
-#include <limits.h>
+#include <stdlib.h>
 
-// Variavel estatica usada para armazenar resultados de calculos anteriores
-static long long int passos[10000000];
-
-long long int collatz (long long int n){
+long long int collatz (long long int n, long long int *P){
 
 	/*  Funçao que faz uso de recursao e momorizaçao para calcular o numero de 
 		passos para um numero n tender a 1 na conjectura de Collatz, retorando 
@@ -15,24 +10,28 @@ long long int collatz (long long int n){
 	// Calcula de maneira interativa caso o numero seja maior que 10000000, no 
 	// caso, maior que o numero de posiçoes que podem ser armazenado na memoria.
 	long long int r = 0;
-	while(n >= 10000000){
+	while(n >= 1000000){
 		if (n%2 == 0) n = n/2; 
 		else n = n*3 + 1;
 		r++;
 	}
 
 	// Uso da momerizaçao e base da recursao.
-	if (passos[n] != 0 || n == 1) return passos[n];
+	if (P[n] != 0 || n == 1) return P[n];
 
 	// Calculo recursivo do numero de passos, sendo que a posiçao n da array passos
 	// armazena o numero de passos necessarios para o elemento n tender a 1. 
-	if (n%2 == 0) passos[n] = collatz(n/2) + 1 + r;
-	else passos[n] = collatz(3*n + 1) + 1 + r;
+	if (n%2 == 0) P[n] = collatz(n/2, P) + 1 + r;
+	else P[n] = collatz(3*n + 1, P) + 1 + r;
 
-	return passos[n];
+	return P[n];
 }
 
 int main(){	
+	// Variavel usada para armazenar resultados de calculos anteriores
+	long long int *passos;
+	passos = malloc(1000000 * sizeof(long long int));
+
 	// Recebendo intervalo no qual sera calculada a conjectura.
 	long long int ini, fim;
 	scanf("%Li%Li", &ini, &fim);
@@ -43,6 +42,11 @@ int main(){
 
 	// Imprimindo o resultado para cada elemento do intervalo.
 	for (long long int n = ini; n <= fim; n++){
-		printf("%Li\n", collatz(n));
+		printf("%Li\n", collatz(n, passos));
 	}
+
+	// Liberando memoria 
+	free(passos);
+
+	return 0;
 }
